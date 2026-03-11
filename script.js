db410e15c97609bf8ed9f238c5f64038b7cf0b3c
fasnetworkmarketing-dev <fasnetworkmarketing@gmail.com>
2026-03-12 01:05:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+particlesJS("particles-js", {
+
+particles: {
+
+number: { value: 80 },
+
+size: { value: 3 },
+
+color: { value: "#8f5cff" },
+
+line_linked: {
+
+enable: true,
+distance: 150,
+color: "#4d8cff",
+opacity: 0.3
+
+},
+
+move: {
+
+enable: true,
+speed: 2
+
+}
+
+},
+
+interactivity: {
+
+events: {
+
+onhover: {
+
+enable: true,
+mode: "repulse"
+
+}
+
+}
+
+}
+
+});
